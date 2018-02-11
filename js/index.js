3e41d5f6dcd0ce7fcc9e13fda8ff5b3ba8416be7
Frank DelPidio <frankdelpidio@gmail.com>
2018-02-11 11:42:20 -0500

    Configure JS for home page

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $('.page').on('click', function(event) {
+    var link, content
+    event.preventDefault();
+
+    link = $(event.target)
+    $('a').removeClass('active')
+    link.addClass('active')
+
+    start_or_stop_video(link.text())
+
+    $('.content').hide()
+    content = '.' + link.data('content')
+    $(content).show()
+  })
+})
+
+function start_or_stop_video(text) {
+  if (text == 'Home' ) {
+    startVideo()
+  } else {
+    stopVideo()
+  }
+}
+
+function stopVideo() {
+  var iframe = $('iframe').get(0)
+  iframe.src = iframe.src.replace(/[?].+/, '')
+}
+
+function startVideo(src) {
+  var iframe = $('iframe').get(0)
+  iframe.src = iframe.src + '?autoplay=1'
+}
