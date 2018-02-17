c1f47f2f370d2004029d7d18bf8225ccf9708d2c
Frank DelPidio <frankdelpidio@gmail.com>
2018-02-17 09:11:42 -0500

    Collapse nav bar when tab is clicked

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
-  $('.page').on('click', function(event) {
-    var link, content
-    event.preventDefault();
-
-    $('.navbar-collapse').removeClass('show')
-
-    link = $(event.target)
-    $('a').removeClass('active')
-    link.addClass('active')
-
-    start_or_stop_video(link.text())
-
-    $('.content').hide()
-    content = '.' + link.data('content')
-    $(content).show()
-  })
+  $( '.page' ).on('click', displayPage)
+  $( 'nav a' ).on('click', collapseNavBar)
 })
+
+function displayPage(event) {
+  var link, content
+  event.preventDefault();
+
+  link = $(event.target)
+  $('a').removeClass('active')
+  link.addClass('active')
+
+  start_or_stop_video(link.text())
+
+  $('.content').hide()
+  content = '.' + link.data('content')
+  $(content).show()
+}
 
 function start_or_stop_video(text) {
   var iframe = $('iframe').get(0)
@@ -35,4 +36,9 @@ function stopVideo() {
 function startVideo(src) {
   var iframe = $('iframe').get(0)
   iframe.src = iframe.src + '?autoplay=1'
+}
+
+function collapseNavBar(event) {
+  if ($(event.target).hasClass('dropdown-toggle')) { return }
+  $('.navbar-toggler').click()
 }
